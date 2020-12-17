6764b250612266611cf07bd4b139bc8a34332e3d
TheDarkLach <11thunderstorm11@gmail.com>
2020-12-16 16:44:06 -0800

    proto

--- main.cpp
+++ main.cpp
@@ -7,6 +7,26 @@
 
 using namespace std;
 
+struct Trunk {
+  Trunk *prev;
+  char* str;
+
+  Trunk(Trunk *prev, char* str) {
+    this -> prev = prev;
+    this -> str = str;
+  }
+};
+
+void parseInput(char input[]);
+void addNode(int value, Node* current);
+void printTree(Node* root, Trunk *prev, bool isLeft);
+void showTrunks(Trunk *p);
+Node* search(int value, Node* current);
+void remove(int value, Node* current);
+void shiftUp(Node* current, bool LR);
+Node* findLeast(Node* current, bool LR);
+
+
 int main()
 {
 
