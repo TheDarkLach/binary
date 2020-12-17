ffa3081452ef4b18e5f31f36210d5cabaafa8667
TheDarkLach <11thunderstorm11@gmail.com>
2020-12-16 17:13:58 -0800

    need print and remove

--- main.cpp
+++ main.cpp
@@ -132,3 +132,129 @@ int main()
   
   return 0;
 }
+
+void parseInput(char input[]) {//remove spaces between the chars, make int pointer (array)
+  /*This works by finding a space and adding a pointer, then moving forward in the array until it finds another
+   *space. It then adds another pointer and adds the characters between the spaces to a new char array, which is then
+   *turned to an int and added to the int pointer. This method was originally created by Zareef Amyeen, but I modified
+   *it for use with an int pointer instead of a vector. 
+   */
+  int counter;
+  int* parsed;
+  int pointers[2];
+  int value;
+  int* temp;
+  counter = 1;
+  pointers[0] = -1;
+  for (int i = 0; i < strlen(input); i++) {
+    if (input[i] == ' ') {
+      pointers[1] = i;
+      int j = 0;
+      char* newArray = new char[pointers[1] - pointers[0]];
+      for (int i = pointers[0] + 1; i < pointers[1]; i++) {
+	newArray[j] = input[i];
+	//cout << newArray[i] << endl;
+	j = j + 1;
+      }
+      newArray[j] = '\0';
+      temp = parsed;
+      parsed = new int[counter];
+      if (counter > 1) {
+	for (int i = 0; i < counter - 1; i++) {
+	  parsed[i] = temp[i];
+	  //cout << parsed[i] << endl;
+}
+      }
+      parsed[counter - 1] = atoi(newArray);
+      /*for (int i = 0; i < counter; i++) {
+	cout << endl << parsed[i] << endl;
+	}*/
+      counter = counter + 1;
+      pointers[0] = pointers[1];
+    }
+  }
+  char* newArray = new char[strlen(input) - pointers[0]];
+  int j = 0;
+  for (int i = pointers[0] + 1; i < strlen(input); i++) {
+    newArray[j] = input[i];
+    //cout << newArray[j];
+    j = j + 1;
+  }
+  newArray[j] = '\0';
+  temp = parsed;
+  parsed = new int[counter];
+  if (counter > 1) {
+    for (int i = 0; i < counter - 1; i++) {
+      parsed[i] = temp[i];
+      //cout << parsed[i] << endl;
+    }
+  }
+  parsed[counter - 1] = atoi(newArray);
+  /*for (int i = 0; i < counter; i++) {
+    cout << endl << parsed[i] << endl;
+    }*/
+  //cout << "works" << endl;
+  for (int i = 0; i < counter; i++) {//add all input to the tree
+    //cout << "fault?" << endl;
+    addNode(parsed[i], head);
+  }
+}
+
+void addNode(int value, Node* current) {//method for adding nodes
+  //cout << "Works?" << endl;
+  if (head == NULL) {//if the tree doesn't exist
+    head = new Node();
+    head-> setValue(value);
+  }
+  else {//if it does exist
+    if (value > current -> getValue()) {//if the value is larger than the current node
+      if (current -> getRight() == NULL) {//if the node doesn't have a right child
+	current -> setRight(new Node());
+	current -> getRight() -> setParent(current);
+	current -> getRight() -> setValue(value);
+      }
+      else
+      {
+	//if not null, recursively call next right
+	addNode(value, current -> getRight());
+      }
+    }
+      
+    else if (value < current -> getValue())
+    {
+      //if the value is less than the current node
+      if (current -> getLeft() == NULL)
+      {
+	//if the node doesn't have a left child
+	current -> setLeft(new Node());
+	current-> getLeft() -> setParent(current);
+	current -> getLeft() -> setValue(value);
+      }
+      else
+      {
+	//if not null, recursively call next left
+	addNode(value, current -> getLeft());
+      }
+
+    }
+    else
+    {
+      //if the value is a duplicate
+      cout << "You cannot have duplicate values." << endl;
+    }
+  }
+}
+
+//https://www.techiedelight.com/c-program-print-binary-tree/
+//i think i couldve put this with the prototypes but idk if it matters
+void showTrunks(Trunk *p)
+{
+  //helper method for printing
+  if (p == NULL)
+  {
+    return;
+  }
+  showTrunks(p -> prev);
+
+  cout << p -> str;
+}
